a3d535b90886b5a88db525d686df5fed5b0973dc
Kevin Siml <appzer@users.noreply.github.com>
2017-02-23 22:37:34 +0100

    Update module.php

--- Pushsafer/module.php
+++ Pushsafer/module.php
@@ -3,7 +3,7 @@
  
         public function __construct($InstanceID) {
             parent::__construct($InstanceID);
-         }
+        }
  
         public function Create() {
             parent::Create();
@@ -24,43 +24,42 @@
  
         public function SendAlert($message) {
             $POST_Para["k"] = $this->ReadPropertyString("privatekey");
-			$POST_Para["m"] = $message;
-			$POST_Para["t"] = $this->ReadPropertyString("title");
-			$POST_Para["d"] = $this->ReadPropertyString("device");
-			$POST_Para["i"] = $this->ReadPropertyString("icon");
-			$POST_Para["s"] = $this->ReadPropertyString("sound");
-			$POST_Para["v"] = $this->ReadPropertyString("vibration");
-			$POST_Para["l"] = $this->ReadPropertyString("time2live");
-			$POST_Para["u"] = $this->ReadPropertyString("url");
-			$POST_Para["ut"] = $this->ReadPropertyString("urltitle");
-			APIRequest($POST_Para);
-        }
-		
-		private function APIRequest($POST_Para) {
-			curl_setopt_array($ch = curl_init(), array(
-				CURLOPT_URL => "https://www.pushsafer.com/api",
-				CURLOPT_SSL_VERIFYPEER => false,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_HEADER => true,
-				CURLOPT_HTTPHEADER => array('Content-type: application/x-www-form-urlencoded'),
-				CURLOPT_POSTFIELDS => $POST_Para,
-			));
-			$response = curl_exec($ch);
-			curl_close($ch);
-			if($response === false) {
-				IPS_LogMessage("Pushsafer", "Connection Error!");
-			} else {
-				list($header, $body) = explode("\r\n\r\n", $response, 2);
-				//echo $header."\r\n";
-				//echo "**".$body."**\r\n";
-				$result = json_decode($body, 1);
-				(isset($result['success'])) ? $success = $result['success'] : $success = null;
-				(isset($result['status'])) ? $status = $result['status'] : $status = null;
-				(isset($result['available'])) ? $available = $result['available'] : $available = null;
+	    $POST_Para["m"] = $message;
+	    $POST_Para["t"] = $this->ReadPropertyString("title");
+	    $POST_Para["d"] = $this->ReadPropertyString("device");
+	    $POST_Para["i"] = $this->ReadPropertyString("icon");
+	    $POST_Para["s"] = $this->ReadPropertyString("sound");
+	    $POST_Para["v"] = $this->ReadPropertyString("vibration");
+	    $POST_Para["l"] = $this->ReadPropertyString("time2live");
+	    $POST_Para["u"] = $this->ReadPropertyString("url");
+	    $POST_Para["ut"] = $this->ReadPropertyString("urltitle");
+			
+	    curl_setopt_array($ch = curl_init(), array(
+	        CURLOPT_URL => "https://www.pushsafer.com/api",
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HEADER => true,
+		CURLOPT_HTTPHEADER => array('Content-type: application/x-www-form-urlencoded'),
+		CURLOPT_POSTFIELDS => $POST_Para,
+	    ));
+			
+	    $response = curl_exec($ch);
+	    curl_close($ch);
+			
+	    if($response === false) {
+	        IPS_LogMessage("Pushsafer", "Connection Error!");
+	    } else {
+		list($header, $body) = explode("\r\n\r\n", $response, 2);
+		//echo $header."\r\n";
+		//echo "**".$body."**\r\n";
+		$result = json_decode($body, 1);
+		(isset($result['success'])) ? $success = $result['success'] : $success = null;
+		(isset($result['status'])) ? $status = $result['status'] : $status = null;
+		(isset($result['available'])) ? $available = $result['available'] : $available = null;
 	
-				//echo $success." : ".$status." : ".$available;
-				IPS_LogMessage("Pushsafer", "Send : ".$success." : ".$status." : ".$available);
-			}
-		}
+		//echo $success." : ".$status." : ".$available;
+		IPS_LogMessage("Pushsafer", "Send : ".$success." : ".$status." : ".$available);
+	    }
+        }
     }
 ?>
