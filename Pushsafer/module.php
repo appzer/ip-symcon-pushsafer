8b55ef61c9387fcbae0911c304d73d330469ed70
Kevin Siml <appzer@users.noreply.github.com>
2017-02-24 11:11:36 +0100

    Update module.php

--- Pushsafer/module.php
+++ Pushsafer/module.php
@@ -1,65 +1,85 @@
 <?
-    class Pushsafer extends IPSModule {
- 
-        public function __construct($InstanceID) {
-            parent::__construct($InstanceID);
-        }
- 
-        public function Create() {
-            parent::Create();
-			$this->RegisterPropertyString("privatekey", "XXXXXXXXXXXXXXXXXXXX");
-			$this->RegisterPropertyString("title", "IP-Symcon");
-			$this->RegisterPropertyString("device", "a");
-			$this->RegisterPropertyString("icon", "1");
-			$this->RegisterPropertyString("sound", "1");
-			$this->RegisterPropertyString("vibration", "3");
-			$this->RegisterPropertyString("time2live", "0");
-			$this->RegisterPropertyString("url", "https://www.pushsafer.com");
-			$this->RegisterPropertyString("urltitle", "Open Pushsafer.com");
-        }
- 
-        public function ApplyChanges() {
-            parent::ApplyChanges();
-        }
- 
-        public function SendAlert($message) {
-            $POST_Para["k"] = $this->ReadPropertyString("privatekey");
-	    $POST_Para["m"] = $message;
-	    $POST_Para["t"] = $this->ReadPropertyString("title");
-	    $POST_Para["d"] = $this->ReadPropertyString("device");
-	    $POST_Para["i"] = $this->ReadPropertyString("icon");
-	    $POST_Para["s"] = $this->ReadPropertyString("sound");
-	    $POST_Para["v"] = $this->ReadPropertyString("vibration");
-	    $POST_Para["l"] = $this->ReadPropertyString("time2live");
-	    $POST_Para["u"] = $this->ReadPropertyString("url");
-	    $POST_Para["ut"] = $this->ReadPropertyString("urltitle");
-			
-	    curl_setopt_array($ch = curl_init(), array(
-	        CURLOPT_URL => "https://www.pushsafer.com/api",
-		CURLOPT_SSL_VERIFYPEER => false,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_HEADER => true,
-		//CURLOPT_HTTPHEADER => array('Content-type: application/x-www-form-urlencoded'),
-		CURLOPT_POSTFIELDS => $POST_Para,
-	    ));
-			
-	    $response = curl_exec($ch);
-	    curl_close($ch);
-			
-	    if($response === false) {
-	        IPS_LogMessage("Pushsafer", "Connection Error!");
-	    } else {
-		list($header, $body) = explode("\r\n\r\n", $response, 2);
-		//echo $header."\r\n";
-		//echo "**".$body."**\r\n";
-		$result = json_decode($body, 1);
-		(isset($result['success'])) ? $success = $result['success'] : $success = null;
-		(isset($result['status'])) ? $status = $result['status'] : $status = null;
-		(isset($result['available'])) ? $available = $result['available'] : $available = null;
+// Pushsafer Push Notification Service
+class Pushsafer extends IPSModule
+{
+	private $privatekey = "";
+	private $title = "";
+	private $device = "";
+	private $icon = "";
+	private $sound = "";
+	private $vibration = "";
+	private $time2live = "";
+	private $url = "";
+	private $urltitle = "";
+	    
+	public function Create()
+	{
+		//Never delete this line!
+        	parent::Create();
+		
+		//These lines are parsed on Symcon Startup or Instance creation
+		//You cannot use variables here. Just static values.
+		
+		$this->RegisterPropertyString("privatekey", "XXXXXXXXXXXXXXXXXXXX");
+		$this->RegisterPropertyString("title", "IP-Symcon");
+		$this->RegisterPropertyString("device", "a");
+		$this->RegisterPropertyString("icon", "1");
+		$this->RegisterPropertyString("sound", "1");
+		$this->RegisterPropertyString("vibration", "3");
+		$this->RegisterPropertyString("time2live", "0");
+		$this->RegisterPropertyString("url", "https://www.pushsafer.com");
+		$this->RegisterPropertyString("urltitle", "Open Pushsafer.com");
+   	}
 	
-		//echo $success." : ".$status." : ".$available;
-		IPS_LogMessage("Pushsafer", "Send : ".$success." : ".$status." : ".$available);
-	    }
-        }
-    }
+	public function ApplyChanges() 
+	{
+        	// Diese Zeile nicht löschen
+        	parent::ApplyChanges();
+	}
+	
+	########## private functions ##########
+	public function TestMessage() 
+	{	
+		$this->SendMessage("Test");
+	}
+	
+	########## public functions ##########
+	public function SendMessage(string $message) 
+	{
+		@curl_setopt_array($ch = curl_init(), array(
+			CURLOPT_URL => "https://www.pushsafer.com/api",
+			CURLOPT_POSTFIELDS => array(
+				"k" => IPS_GetProperty($this->InstanceID,"privatekey"),
+				"t" => IPS_GetProperty($this->InstanceID,"title"),
+				"d" => IPS_GetProperty($this->InstanceID,"device"),
+				"i" => IPS_GetProperty($this->InstanceID,"icon"),
+				"s" => IPS_GetProperty($this->InstanceID,"sound"),
+				"v" => IPS_GetProperty($this->InstanceID,"vibration"),
+				"l" => IPS_GetProperty($this->InstanceID,"time2live"),
+				"u" => IPS_GetProperty($this->InstanceID,"url"),
+				"ut" => IPS_GetProperty($this->InstanceID,"urltitle"),
+				"m" => $message
+			),
+			CURLOPT_SAFE_UPLOAD => true,
+			CURLOPT_RETURNTRANSFER => true
+		));
+		
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		if($response === false) {
+			IPS_LogMessage("Pushsafer", "Connection Error!");
+		} else {
+			list($header, $body) = explode("\r\n\r\n", $response, 2);
+			//echo $header."\r\n";
+			//echo "**".$body."**\r\n";
+			$result = json_decode($body, 1);
+			(isset($result['success'])) ? $success = $result['success'] : $success = null;
+			(isset($result['status'])) ? $status = $result['status'] : $status = null;
+			(isset($result['available'])) ? $available = $result['available'] : $available = null;
+			//echo $success." : ".$status." : ".$available;
+			IPS_LogMessage("Pushsafer", "Send : ".$success." : ".$status." : ".$available);
+		}		
+	}
+}
 ?>
